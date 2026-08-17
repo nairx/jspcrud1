3a0a6145adadf87ce33d52c7217c95508334e288
nairx <nairxprv@gmail.com>
2026-08-17 15:50:22 +0530

    first commit

---
+++ src/main/java/com/example/jspcrud1/Jspcrud1Application.java
@@ -0,0 +1,13 @@
+package com.example.jspcrud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jspcrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jspcrud1Application.class, args);
+	}
+
+}
